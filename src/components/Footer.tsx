503a370af60faef746d7e509dcbd238566587088
Jared Dube <jdube3@academic.rrc.ca>
2026-02-01 11:48:33 -0600

    restructure: migrated to a new directory structure

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-    const currentYear = new Date().getFullYear();
-    
-    return (
-        <footer id="copyright">
-            Copyright Pixell River Financial {currentYear}
-        </footer>
-    );
-}
