59529e70d648df1b5b5abbd5df2118a9298f5914
Jared Dube <jdube3@academic.rrc.ca>
2026-02-01 11:54:38 -0600

    refactor: rewrote to use a basic router setup with different pages that can be accessed via the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Page } from './components/Page'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/common/Layout";
+import { EmployeePage } from './components/pages/EmployeePage'
+import { Organization } from './components/pages/Organization'
 import { departments } from './data/employeeData'
 import './App.css'
 
 function App() {
-  return <Page departments={departments} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<EmployeePage departments={departments} />} />
+          <Route path="employees" element={<EmployeePage departments={departments} />} />
+          <Route path="organization" element={<Organization />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
